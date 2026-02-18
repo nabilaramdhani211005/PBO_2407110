357cbd967a7be81e331d74c28ff6856981eef39b
nabilaramdhani211005 <nabilaramdhani211005@gmail.com>
2026-02-18 12:01:50 +0700

    latihan_2 hari rabu 18/02/26

--- Latihan _2407110/src/latihan_pbo/latihan_1/main.java
+++ Latihan _2407110/src/latihan_pbo/latihan_1/main.java
@@ -3,17 +3,36 @@ package latihan_pbo.latihan_1;
 class Cafe{
     String nama;
     String alamat;
+   
 
     //constructor
     Cafe (String nama, String alamat ){
         this.nama = nama;
         this.alamat = alamat;
+    
         System.out.println( "nama cafenya adalah: " + nama);
         System.out.println("alamat cafenya dimana: " + alamat);
         System.out.println("=================");
 
-
+        //method
+       
     }
+     public String getNama() {
+        return nama;
+    }
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+    public String getAlamat() {
+        return alamat;
+    }
+    public void setAlamat(String alamat) {
+        this.alamat = alamat;
+    }
+     void buka(){
+            System.out.println("============");
+            System.out.println("cafe sudah buka");
+        }
 
 
 
@@ -21,11 +40,10 @@ class Cafe{
 
 public class main {
     public static void main(String[] args) {
-    
-        
 
         Cafe Cafe1 = new Cafe( "kenangan", "cirebon");
         Cafe Cafe2 = new Cafe(  "Tabo", "cirebon");
         Cafe Cafe3 = new Cafe(  "Tomoro", "cirebon");
+        Cafe1.buka();
     }
 }
